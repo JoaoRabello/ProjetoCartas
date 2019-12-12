a1cf6cad12642b3ec6da4a28ec8fc39a2fce400e
João Vitor Rabello de Araujo <rabellois@hotmail.com>
2019-12-12 17:23:00 -0300

    Triagem Started
    -Camera control with clicks

---
+++ Projeto Cartas/Assets/Scripts/Entrega/Player/PlayerMovement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private CameraControl _cameraControl;
+    private GameObject _camera;
+
+    [SerializeField] private float _speed;
+    private float _actualSpeed;
+    private float _bikeSpeed;
+    private bool _nextToBike = false;
+    private bool _onBike = false;
+    private GameObject _bike;
+
+    void Start()
+    {
+        _cameraControl = FindObjectOfType<CameraControl>();
+        _camera = _cameraControl.ActualCamera;
+
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _actualSpeed = _speed;
+        _bikeSpeed = _speed * 2f;
+    }
+
+    void Update()
+    {
+        _camera = _cameraControl.ActualCamera;
+
+        BikeControl();
+    }
+
+    private void FixedUpdate()
+    {
+        NormalControl();
+    }
+
+    private void NormalControl()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 camF = _camera.transform.forward;
+        Vector3 camR = _camera.transform.right;
+
+        camF.y = 0;
+        camR.y = 0;
+        camF = camF.normalized;
+        camR = camR.normalized;
+
+        Vector3 horizontalMovement = input.x * camR * Time.deltaTime * _actualSpeed;
+        Vector3 verticalMovement = input.y * camF * Time.deltaTime * _actualSpeed;
+
+        _rigidbody.MovePosition(transform.position + horizontalMovement + verticalMovement);
+    }
+
+    private void BikeControl()
+    {
+        if (_nextToBike && !_onBike && Input.GetKeyDown(KeyCode.E))
+        {
+            _onBike = true;
+            _actualSpeed = _bikeSpeed;
+            _bike.transform.parent = transform;
+
+        }
+        else
+        {
+            if (_onBike && Input.GetKeyDown(KeyCode.E))
+            {
+                _onBike = false;
+                _actualSpeed = _speed;
+                _bike.transform.parent = null;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CameraArea"))
+        {
+            _cameraControl.CameraChange(other.GetComponent<CameraArea>().cameraNumber);
+        }
+        else
+        {
+            if (other.CompareTag("BikeArea"))
+            {
+                _nextToBike = true;
+                _bike = other.gameObject;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("BikeArea"))
+        {
+            _nextToBike = false;
+        }
+    }
+}
---
+++ Projeto Cartas/Assets/Scripts/Triagem/MouseControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] Transform firstCamPos;
+    [SerializeField] Transform tableCamPos;
+    [SerializeField] Transform bagCamPos;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                CheckClickedObject(hit.collider.tag);
+            }
+        }
+    }
+
+    private void CheckClickedObject(string tag)
+    {
+        switch (tag)
+        {
+            case "Table":
+                StopAllCoroutines();
+                StartCoroutine(MoveToPosition(transform, tableCamPos, 2f));
+                break;
+            case "Bag":
+                StopAllCoroutines();
+                StartCoroutine(MoveToPosition(transform, bagCamPos, 2f));
+                break;
+            default:
+                StopAllCoroutines();
+                StartCoroutine(MoveToPosition(transform, firstCamPos, 1.5f));
+                break;
+        }
+    }
+
+    IEnumerator MoveToPosition(Transform initialPoint, Transform targetPoint, float timeOffset)
+    {
+        float startTime = Time.time;
+        while (Time.time < startTime + timeOffset)
+        {
+            transform.position = Vector3.Lerp(initialPoint.position, targetPoint.position, (Time.time - startTime) / timeOffset);
+            transform.rotation = Quaternion.Lerp(initialPoint.rotation, targetPoint.rotation, (Time.time - startTime) / timeOffset);
+            yield return null;
+        }
+        cam.transform.position = targetPoint.position;
+    }
+}
